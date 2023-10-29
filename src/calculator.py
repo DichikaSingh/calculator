b24f2b5762bae11696ef9932e85211ecc63c964e
DichikaSingh <149289432+DichikaSingh@users.noreply.github.com>
2023-10-29 22:49:53 +0530

    Add files via upload

--- src/calculator.py
+++ src/calculator.py
@@ -1,1 +1,24 @@
+import io
 
+def calculate():
+    operator= input("Choose oprator to perform opration(+, -, *, /, **)\n")
+    First_num= float(input("First number \n"))
+    Second_num= float(input("Second number \n"))
+    result= 0
+    if operator== "+":
+        result = First_num + Second_num
+        print(round(result,3))  
+    elif operator== "-":
+        result= First_num - Second_num
+        print(round(result,3)) 
+    elif operator== "*":
+        result= First_num * Second_num
+        print(round(result,3)) 
+    elif operator== "/":
+        result= First_num / Second_num
+        print(round(result,3))
+    elif operator== "**":
+        result=First_num ** Second_num
+        print(round(result))
+    else :
+        print("Invalid operator, choose again")
