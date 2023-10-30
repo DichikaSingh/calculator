37833ced26f4f6fb04c7645c2930572d7265d282
DichikaSingh <149289432+DichikaSingh@users.noreply.github.com>
2023-10-30 10:09:38 +0530

    Add files via upload

--- src/calculator.py
+++ src/calculator.py
@@ -1,24 +1,44 @@
 import io
 
+
 def calculate():
-    operator= input("Choose oprator to perform opration(+, -, *, /, **)\n")
-    First_num= float(input("First number \n"))
-    Second_num= float(input("Second number \n"))
-    result= 0
-    if operator== "+":
-        result = First_num + Second_num
-        print(round(result,3))  
-    elif operator== "-":
-        result= First_num - Second_num
-        print(round(result,3)) 
-    elif operator== "*":
-        result= First_num * Second_num
-        print(round(result,3)) 
-    elif operator== "/":
-        result= First_num / Second_num
-        print(round(result,3))
-    elif operator== "**":
-        result=First_num ** Second_num
-        print(round(result))
-    else :
-        print("Invalid operator, choose again")
+    while True:
+        operator= input("Choose oprator to perform opration(+, -, *, /, **)\n")
+        First_num= float(input("First number \n"))
+        Second_num= float(input("Second number \n"))
+        result= 0
+
+        if operator== "+":
+            result = First_num + Second_num
+            print(round(result,3)) 
+
+        elif operator== "-":
+            result= First_num - Second_num
+            print(round(result,3)) 
+                
+        elif operator== "*":
+            result= First_num * Second_num
+            print(round(result,3)) 
+
+        elif operator== "/":
+            result= First_num / Second_num
+            print(round(result,3))
+
+        elif operator== "**":
+            result=First_num ** Second_num
+            print(round(result))
+        else:
+            print("invalid operator")
+ 
+        Calculate_again=input("Do you want to calculate again?(Yes/No)")
+        if Calculate_again.lower()=="Yes":
+            calculate()
+        elif Calculate_again.lower()=="no":
+            break        
+            
+   
+
+        
+
+
+
